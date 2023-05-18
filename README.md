d306d427d3dd8ab327644007f25d8d67d52f9c31
Adrien Coudour <adrien.coudour@etu.uca.fr>
2023-05-18 16:49:23 +0200

    Modification du README

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # WholeSalerService
+## Projet Cloud Licence Pro Web
+Made By Thomas Da Silva Mendonca With Adrien Coudour
+
+Whole Saler Service:
+
+* Appelé par le ShoppingService lorsque le stock est inférieur à la quantité commandé.
+* Renvoie que la demande à été efféctué.
+* Appel le StockService pour ajouter dans le stock la quantité commandé.
--- src/main/java/com/stock/wholeSalerService/WholeSalerServiceController.java
+++ src/main/java/com/stock/wholeSalerService/WholeSalerServiceController.java
@@ -6,7 +6,6 @@ package com.stock.wholeSalerService;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,11 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
  * @author adcoudour
  */
 @RestController
-@RequestMapping("/wholeSaler")
-public class WholeSalerService {
-    public WholeSalerService() {
+public class WholeSalerServiceController {
+    public WholeSalerServiceController() {
         
     }
+    
+    @GetMapping("/ws")
+	public String index() {
+		return "WholeSaler";
+	}
     
     /**
      * Cette méthode sert à faire une requête pour vérifier que le stock du livre est superieur 
@@ -28,9 +31,9 @@ public class WholeSalerService {
      * @param corr Id de corrélation composé de isbn et account
      * @return message de validation de la demande du livre
      */
-    @GetMapping("/{key}/{isbn}/{quantity}/{corr}")
+    @GetMapping("/wholesaler_service/command/{key}/{isbn}/{quantity}/{corr}")
     public ResponseEntity<String> WholeSalerRequest(int key, String isbn,int quantity,String corr){
         // Appel du stockService pour ajouter des livres sur l'isbn
-        return ResponseEntity.ok("WholeSale order sent"+key);
+        return ResponseEntity.ok("WholeSale order sent ");
     }
 }
